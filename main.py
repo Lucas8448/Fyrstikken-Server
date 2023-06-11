e234cbe9e00e3f6fb973103653b3ca74290ed556
Lucas Bateson <lucas.bateson@gmail.com>
2023-06-11 15:18:28 +0200

    Function updates to client

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask_cors import CORS
 from flask import *
 import os
 import json
+import secrets
 
 app = Flask(__name__)
 CORS(app)
 
+tokens = {}
+
 @app.route('/')
 def home():
-  return 'Hello World!'
-
+    return 'Hello World!'
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -21,32 +23,46 @@ def login():
         with open('./storage/users/' + username + '.json', 'r') as f:
             data = json.load(f)
             if data["password"] == password:
-                return jsonify({"success": True})
+                token = secrets.token_hex(16)
+                tokens[username] = token
+                return jsonify({"success": True, "token": token})
             else:
                 return jsonify({"success": False})
     else:
         return jsonify({"success": False})
 
-@app.route('/vote')
+
+@app.route('/vote', methods=['POST'])
 def vote():
-  username = request["username"]
-  password = request["password"]
-  if os.path.exists('./storage/users/' + username + '.json'):
-    with open('./storage/users/' + username + '.json', 'r') as f:
-      data = json.load(f)
-      if data["password"] == password:
-        if data["voted"] == True:
-          return {"success":False}
-        else:
-          data["voted"] = True
-          data["votes"] = request["votes"]
-          with open('./storage/users/' + username + '.json', 'w') as f:
-            json.dump(data, f)
-          return {"success":True}
-      else:
-        return {"success":False}
-  else:
-    return {"success":False}
+    auth_header = request.headers.get('Authorization')
+
+    if not auth_header:
+        return jsonify({"success": False, "message": "Token is missing"})
+
+    token = auth_header.split(" ")[1]
+    username = None
+
+    for user, user_token in tokens.items():
+        if user_token == token:
+            username = user
+            break
+
+    if username is None:
+        return jsonify({"success": False, "message": "Token is invalid"})
+
+    if os.path.exists('./storage/users/' + username + '.json'):
+        with open('./storage/users/' + username + '.json', 'r') as f:
+            data = json.load(f)
+            if data["voted"] == True:
+                return jsonify({"success": False})
+            else:
+                data["voted"] = True
+                data["votes"] = request.json["votes"]
+                with open('./storage/users/' + username + '.json', 'w') as f:
+                    json.dump(data, f)
+                return jsonify({"success": True})
+    else:
+        return jsonify({"success": False})
 
 
 if __name__ == "__main__":
