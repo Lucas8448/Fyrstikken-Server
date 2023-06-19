0c0e25c73ea00c65d2f205da914b3bc3a2cc8568
Lucas Bateson <lucas.bateson@gmail.com>
2023-06-19 19:07:35 +0200

    More logins per page, and character clarification

--- create_users.py
+++ create_users.py
@@ -4,7 +4,7 @@ from fpdf import FPDF
 import random
 import string
 
-user_count = 2000
+user_count = 400
 
 users = './storage/users/'
 pdfs = './storage/pdfs/'
@@ -15,15 +15,21 @@ if not os.path.exists(users):
 if not os.path.exists(pdfs):
     os.makedirs(pdfs)
 
+
 def randomstring():
-    return ''.join(random.choice(string.ascii_letters) for i in range(8))
+    string = ''.join(random.choice(string.ascii_letters) for i in range(8))
+    # replace I with i and l with L
+    string = string.replace('I', 'i')
+    string = string.replace('l', 'L')
+    return string
+
 
 def create_users():
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size = 12)
-
-    positions = [(x, y) for x in [10, 110] for y in range(20, 280, 70)]
+    pdf.set_font("Arial", size=12)
+    positions = [(x, y) for x in [10, 110]
+                 for y in range(20, 280, 40)]
     user_position = 0
     for i in range(user_count):
         username = randomstring()
@@ -31,19 +37,21 @@ def create_users():
 
         with open(os.path.join(users, username + '.json'), 'w') as f:
             json.dump({
-                'username': username, 
-                'password': password, 
-                'voted': False, 
+                'username': username,
+                'password': password,
+                'voted': False,
                 'votes': {
-                    str(j): 0 for j in range(1, 14)
+                    str(j): 0 for j in range(1, 16)
                 }
             }, f)
-        
+
         x, y = positions[user_position]
         pdf.set_xy(x, y)
-        pdf.multi_cell(80, 10, txt = "Brukernavn: " + username, border=1)
-        pdf.set_xy(x, y + 10)
-        pdf.multi_cell(80, 10, txt = "Passord: " + password, border=1)
+        pdf.multi_cell(80, 8, txt="Brukernavn: " + username,
+                       border=1)
+        pdf.set_xy(x, y + 8)
+        pdf.multi_cell(80, 8, txt="Passord: " + password,
+                       border=1) 
 
         user_position += 1
         if user_position == len(positions):
@@ -53,5 +61,6 @@ def create_users():
         print("Created user " + str(i + 1) + " of " + str(user_count))
 
     pdf.output(os.path.join(pdfs, 'users.pdf'))
+
 
 create_users()
